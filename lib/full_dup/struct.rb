871056f19ff3bb39436bc4e665bd80fcc5d72e35
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 20:55:20 -0400

    Cleaned up struct.rb

--- lib/full_dup/struct.rb
+++ lib/full_dup/struct.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+# Add full_dup support to the struct class.
 class Struct
 
   #The full_dup method for structs.
@@ -8,22 +9,32 @@ class Struct
     exclude = full_dup_exclude
 
     if exclude.empty?
-      members.each do |name|
-        value = result[name]
-        value = progress[value.object_id] || value.full_dup(progress)
-        result[name] = value
-      end
+      result.full_dup_no_exclusions(progress)
     else
-      members.each do |name|
-        unless exclude.include?(name)
-          value = result[name]
-          value = progress[value.object_id] || value.full_dup(progress)
-          result[name] = value
-        end
-      end
+      result.full_dup_with_exclusions(progress, exclude)
     end
 
     result
+  end
+
+  # Do a full_dup with no exclusions
+  def full_dup_no_exclusions(progress)
+    members.each do |name|
+      value = self[name]
+      value = progress[value.object_id] || value.full_dup(progress)
+      self[name] = value
+    end
+  end
+
+  # Do a full_dup with exclusions
+  def full_dup_with_exclusions(progress, exclude)
+    members.each do |name|
+      unless exclude.include?(name)
+        value = self[name]
+        value = progress[value.object_id] || value.full_dup(progress)
+        self[name] = value
+      end
+    end
   end
 
 end
