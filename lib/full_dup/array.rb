e0daf45ec99b8dacd1c208fba84a8bedda8ec443
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 21:11:15 -0400

    Cleaned up array.rb. full_dup no longer stinks.

--- lib/full_dup/array.rb
+++ lib/full_dup/array.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+# Add full_dup support to the array class.
 class Array
 
   #The full_dup method for arrays.
@@ -8,22 +9,32 @@ class Array
     exclude = full_dup_exclude
 
     if exclude.empty?
-      each_index do |name|
-        value = result[name]
-        value = progress[value.object_id] || value.full_dup(progress)
-        result[name] = value
-      end
+      result.full_dup_no_exclusions(progress)
     else
-      each_index do |name|
-        unless exclude.include?(name)
-          value = result[name]
-          value = progress[value.object_id] || value.full_dup(progress)
-          result[name] = value
-        end
-      end
+      result.full_dup_with_exclusions(progress, exclude)
     end
 
     result
+  end
+
+  # Do a full_dup with no exclusions
+  def full_dup_no_exclusions(progress)
+    each_index do |name|
+      value = self[name]
+      value = progress[value.object_id] || value.full_dup(progress)
+      self[name] = value
+    end
+  end
+
+  # Do a full_dup with exclusions
+  def full_dup_with_exclusions(progress, exclude)
+    each_index do |name|
+      unless exclude.include?(name)
+        value = self[name]
+        value = progress[value.object_id] || value.full_dup(progress)
+        self[name] = value
+      end
+    end
   end
 
 end
