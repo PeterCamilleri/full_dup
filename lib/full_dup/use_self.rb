f209b35d87e711a9048ec0ff60a26a44f53dde7f
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 20:23:04 -0400

    Cleaned up use_self.rb

--- lib/full_dup/use_self.rb
+++ lib/full_dup/use_self.rb
@@ -1,35 +1,43 @@
 # coding: utf-8
 
+# A mixin for classes where the dup is the object itself.
 module FullDupSelf
   def full_dup(_progress=nil)
     self
   end
 end
 
+# Numerics answer self.
 class Numeric
   include FullDupSelf
 end
 
+# NilClass objects (nil) answer self.
 class NilClass
   include FullDupSelf
 end
 
+# TrueClass objects (true) answer self.
 class TrueClass
   include FullDupSelf
 end
 
+# FalseClass objects (false) answer self.
 class FalseClass
   include FullDupSelf
 end
 
+# Symbols answer self.
 class Symbol
   include FullDupSelf
 end
 
+# Regular expressions answer self.
 class Regexp
   include FullDupSelf
 end
 
+# Threads answer self.
 class Thread
   include FullDupSelf
 end
