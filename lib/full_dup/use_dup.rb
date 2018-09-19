392290405361ad0e39ca8a4c38f9ff05750c5dc5
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 20:28:13 -0400

    Cleaned up use_dup.rb

--- lib/full_dup/use_dup.rb
+++ lib/full_dup/use_dup.rb
@@ -1,19 +1,23 @@
 # coding: utf-8
 
+# The mixin for classes where dup is just simple dup.
 module FullDupDup
   def full_dup(_progress=nil)
     dup
   end
 end
 
+# Strings answer dup.
 class String
   include FullDupDup
 end
 
+# Enumerators answer dup.
 class Enumerator
   include FullDupDup
 end
 
+# MatchData answer dup.
 class MatchData
   include FullDupDup
 end
