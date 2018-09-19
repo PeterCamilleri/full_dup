922f49d893c3a4e3db006936d3abf08f55c764c6
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 21:01:27 -0400

    Cleaned up object.rb

--- lib/full_dup/object.rb
+++ lib/full_dup/object.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+# Add full_dup support to the object class.
 class Object
 
   #By default, no instance variables are excluded.
@@ -13,22 +14,34 @@ class Object
     exclude = full_dup_exclude
 
     if exclude.empty?
-      instance_variables.each do |name|
-        value = result.instance_variable_get(name)
-        value = progress[value.object_id] || value.full_dup(progress)
-        result.instance_variable_set(name, value)
-      end
+      result.full_dup_no_exclusions(progress)
     else
-      instance_variables.each do |name|
-        unless exclude.include?(name)
-          value = result.instance_variable_get(name)
-          value = progress[value.object_id] || value.full_dup(progress)
-          result.instance_variable_set(name, value)
-        end
-      end
+      result.full_dup_with_exclusions(progress, exclude)
     end
 
     result
   end
+
+  # Do a full_dup with no exclusions
+  def full_dup_no_exclusions(progress)
+    instance_variables.each do |name|
+      value = instance_variable_get(name)
+      value = progress[value.object_id] || value.full_dup(progress)
+      instance_variable_set(name, value)
+    end
+  end
+
+  # Do a full_dup with exclusions
+  def full_dup_with_exclusions(progress, exclude)
+    instance_variables.each do |name|
+      unless exclude.include?(name)
+        value = instance_variable_get(name)
+        value = progress[value.object_id] || value.full_dup(progress)
+        instance_variable_set(name, value)
+      end
+    end
+  end
+
+
 
 end
