36fa9055407e70c7fe3ac81a66e7a6689cc8bb27
PeterCamilleri <peter.c.camilleri@gmail.com>
2018-09-18 21:07:13 -0400

    Cleaned up hash.rb

--- lib/full_dup/hash.rb
+++ lib/full_dup/hash.rb
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+# Add full_dup support to the hash class.
 class Hash
 
   #The full_dup method for hashes.
@@ -8,22 +9,32 @@ class Hash
     exclude = full_dup_exclude
 
     if exclude.empty?
-      each_key do |name|
-        value = result[name]
-        value = progress[value.object_id] || value.full_dup(progress)
-        result[name] = value
-      end
+      result.full_dup_no_exclusions(progress)
     else
-      each_key do |name|
-        unless exclude.include?(name)
-          value = result[name]
-          value = progress[value.object_id] || value.full_dup(progress)
-          result[name] = value
-        end
-      end
+      result.full_dup_with_exclusions(progress, exclude)
     end
 
     result
+  end
+
+  # Do a full_dup with no exclusions
+  def full_dup_no_exclusions(progress)
+    each_key do |name|
+      value = self[name]
+      value = progress[value.object_id] || value.full_dup(progress)
+      self[name] = value
+    end
+  end
+
+  # Do a full_dup with exclusions
+  def full_dup_with_exclusions(progress, exclude)
+    each_key do |name|
+      unless exclude.include?(name)
+        value = self[name]
+        value = progress[value.object_id] || value.full_dup(progress)
+        self[name] = value
+      end
+    end
   end
 
 end
--- lib/full_dup/object.rb
+++ lib/full_dup/object.rb
@@ -42,6 +42,4 @@ class Object
     end
   end
 
-
-
 end
